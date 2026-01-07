4d3b4e4c12456de943c79d9e99ff96149f06ee33
Sheena O'Connell <sheena.oconnell@gmail.com>
2026-01-07 05:57:54 +0000

    deploy works

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -132,3 +132,8 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
+
+# Allauth 
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL="https"
