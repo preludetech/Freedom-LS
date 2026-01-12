31541430b84428445a3b8c826033ae39ba36bdd7
Sheena O'Connell <sheena.oconnell@gmail.com>
2026-01-12 14:55:21 +0200

    upgraded create_site function

--- freedom_ls/site_aware_models/management/commands/create_site.py
+++ freedom_ls/site_aware_models/management/commands/create_site.py
@@ -1,6 +1,7 @@
 import djclick as click
 from django.contrib.sites.models import Site
 from django.contrib.auth import get_user_model
+from allauth.account.models import EmailAddress
 
 User = get_user_model()
 
@@ -8,20 +9,36 @@ User = get_user_model()
 @click.command()
 @click.argument("site_name")
 @click.argument("site_domain")
-def command(site_name, site_domain):
+@click.option("--email", default=None, help="Email address for the superuser")
+@click.option("--password", default=None, help="Password for the superuser")
+def command(site_name, site_domain, email, password):
     site, created = Site.objects.get_or_create(
-        domain=site_domain,
-        defaults={"name": site_name},
+        name=site_name,
+        defaults={"domain": site_domain},
     )
 
-    user_email = f"{site_name.lower()}@email.com"
-    if not User.objects.filter(email=user_email).exists():
-        user = User(
-            email=user_email,
-            is_staff=True,
-            is_superuser=True,
-            is_active=True,
-            site=site,
-        )
-        user.set_password(user_email)
+    if site.domain != site_domain:
+        site.domain = site_domain
+
+    user_email = email if email else f"{site_name.lower()}@email.com"
+    user_password = password if password else user_email
+
+    user, user_created = User.objects.get_or_create(
+        email=user_email,
+        defaults={
+            "is_staff": True,
+            "is_superuser": True,
+            "is_active": True,
+            "site": site,
+        }
+    )
+
+    if user_created:
+        user.set_password(user_password)
         user.save()
+
+    EmailAddress.objects.get_or_create(
+        user=user,
+        email=user.email,
+        defaults={"verified": True, "primary": True}
+    )
