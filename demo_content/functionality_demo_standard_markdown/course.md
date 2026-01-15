e4fbb6a374ec196cf9c039b95e184289e506c8be
Oarabile Moima <oarabilemoima@gmail.com>
2026-01-15 09:52:01 +0200

    Expand demo content to show markdown functionaility
    This commit changes the flow of the rendering engine
    to sanitise inputs and to also handle cases of handling
    markdown in custom components.
    This also introduced support for blockquotes and
    markdown tables.
    
    Resolves #26. This issue also taught me more about MD and
    YAML formatting. Ensured that tests continue to pass.
    
    I added a new test course in the demo_content folder
    to display the new changes. Basic compoud interest.
    
    This closes #27.
    
    markdown_utils now uses a context manager.

---
+++ demo_content/functionality_demo_standard_markdown/course.md
@@ -0,0 +1,9 @@
+---
+content_type: COURSE
+description: This will show how all the Standard Markdown goodies show up.
+subtitle: A standard tour of the content system
+title: Standard Markdown - Demo Finance
+uuid: 7e724756-a349-4ef1-924b-ec9c26dcba45
+---
+
+Your competitive advantage is your ability to learn.
--- freedom_ls/content_engine/markdown_utils.py
+++ freedom_ls/content_engine/markdown_utils.py
@@ -12,30 +12,41 @@ import os
 def render_markdown(markdown_text, request, context=None):
     context = context or {}
 
+    md = markdown.Markdown(extensions=["fenced_code", "mdx_headdown", "tables"])
+    md.parser.blockprocessors.deregister("code")  # Disable indented code blocks
+
+    for key in settings.MARKDOWN_ALLOWED_TAGS:
+        md.block_level_elements.append(key)
+
+    rendered_content = md.convert(markdown_text)
+
+    # now clean it
+
     allowed_attribute_tags = settings.MARKDOWN_ALLOWED_TAGS
 
-    allowed_tags = set(allowed_attribute_tags)
+    allowed_tags = deepcopy(nh3.ALLOWED_TAGS)
+    allowed_tags.update(allowed_attribute_tags.keys())
 
     attributes = deepcopy(nh3.ALLOWED_ATTRIBUTES)
     for k, v in allowed_attribute_tags.items():
         attributes[k] = v
 
-    markdown_text = nh3.clean(markdown_text, tags=allowed_tags, attributes=attributes)
-    # [s for s in markdown_text.split("\n") if "c-p" in s]
+    rendered_content = nh3.clean(
+        rendered_content, tags=allowed_tags, attributes=attributes
+    )
+
+    # do the cotton rendering
 
     if settings.MARKDOWN_TEMPLATE_RENDER_ON:
         os.makedirs("/tmp/lms_templates", exist_ok=True)
         temp = tempfile.NamedTemporaryFile(prefix="template_", dir="/tmp/lms_templates")
 
-        temp.write(str.encode(markdown_text))
+        temp.write(str.encode(rendered_content))
         temp.seek(0)
         content = loader.render_to_string(
             Path(temp.name).stem, context=context, request=request, using=None
         )
     else:
-        content = markdown_text
+        content = rendered_content
 
-    md = markdown.Markdown(extensions=["fenced_code", "mdx_headdown"])
-    md.parser.blockprocessors.deregister("code")  # Disable indented code blocks
-
-    return mark_safe(md.convert(content))
+    return mark_safe(content)
