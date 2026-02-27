a11fc0b441da496063f15664c2787f8f2f00f1f0
Sheena O'Connell <sheena.oconnell@gmail.com>
2026-02-27 08:56:43 +0200

    some tidying

--- freedom_ls/educator_interface_depricated/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EducatorInterfaceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "freedom_ls.educator_interface"
-    label = "freedom_ls_educator_interface"
