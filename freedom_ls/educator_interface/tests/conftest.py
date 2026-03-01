559b96eacf946754b241fb2f33de5193a88e3458
Sheena O'Connell <sheena.oconnell@gmail.com>
2026-03-01 05:53:32 +0200

    some cleanup

--- freedom_ls/educator_interface/tests/conftest.py
+++
@@ -1,2 +0,0 @@
-# Fixtures for educator_interface tests are provided by the root conftest
-# and by factories. See freedom_ls/<app>/factories.py
--- freedom_ls/student_progress/tests/conftest.py
+++
@@ -1,2 +0,0 @@
-# Fixtures for student_progress tests are now provided by factories.
-# See freedom_ls/student_progress/factories.py
